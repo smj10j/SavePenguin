713ccb064a517a38a76370e7fe9876399355a38c
Steve Johnson <steve@rewardme.com>
2012-11-01 02:56:42 +0700

    add missing new classes for border=>obstruction rename;

---
+++ LevelHelper/CustomClasses/ToolboxItem_Obstruction.mm
@@ -0,0 +1,52 @@
+//This source file was generated automatically by LevelHelper
+//based on the class template defined by the user.
+//For more info please visit: www.levelhelper.org
+
+
+#import "ToolboxItem_Obstruction.h"
+
+@implementation ToolboxItem_Obstruction
+
+
+@synthesize runningCost;
+@synthesize scale;
+@synthesize placeCost;
+
+
+-(void) dealloc{
+#if __has_feature(objc_arc) && __clang_major__ >= 3
+
+#else
+
+
+[super dealloc];
+
+#endif // __has_feature(objc_arc)
+}
+
++(ToolboxItem_Obstruction*) customClassInstance{
+#if __has_feature(objc_arc) && __clang_major__ >= 3
+return [[ToolboxItem_Obstruction alloc] init];
+#else
+return [[[ToolboxItem_Obstruction alloc] init] autorelease];
+#endif
+}
+
+-(NSString*) className{
+return NSStringFromClass([self class]);
+}
+-(void) setPropertiesFromDictionary:(NSDictionary*)dictionary
+{
+
+	if([dictionary objectForKey:@"runningCost"])
+		[self setRunningCost:[[dictionary objectForKey:@"runningCost"] floatValue]];
+
+	if([dictionary objectForKey:@"scale"])
+		[self setScale:[[dictionary objectForKey:@"scale"] floatValue]];
+
+	if([dictionary objectForKey:@"placeCost"])
+		[self setPlaceCost:[[dictionary objectForKey:@"placeCost"] floatValue]];
+
+}
+
+@end
